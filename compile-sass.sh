e396d29ae012217b06bc0a3c7669cfc59d68a871
Conor Clafferty <conor@thisisbliss.com>
2014-04-24 16:57:06 +0100

    Inform user when Sass is not installed and only issue success message when installation completes successfully

--- compile-sass.sh
+++ compile-sass.sh
@@ -7,15 +7,12 @@ CSS_file="css/main.css"
 # Check is Sass is installed
 if which sass >/dev/null; then
   echo "Reading "$SASS_file" and compiling to "$CSS_file"..."
-  `sass "$SASS_file":"$CSS_file"` &
+  sass "$SASS_file":"$CSS_file"
   echo "Now watching changes in "$SASS_file" and compiling to "$CSS_file"..."
-  `sass --watch "$SASS_file":"$CSS_file"` &
+  sass --watch "$SASS_file":"$CSS_file"
   wait
-fi
 else
-  sudo gem install sass &
-  wait
-  sass -v &
-  echo "Sass is now installed. Please run the script again."
+  echo "Sass is not installed. Installing now..."
+  sudo gem install sass && sass -v && echo "Sass is now installed. Please run the script again."
 fi
  
