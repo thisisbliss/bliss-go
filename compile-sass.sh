dd8d0e29194598a05d273e9cfe1ae79c4d6b48c2
Tom Wilks <tom@thisisbliss.com>
2014-04-24 16:47:07 +0100

    Creates bash file to run automated Sass commands

---
+++ compile-sass.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Run a simple set of Sass commands.
+ 
+SASS_file="css/sass/main.scss"
+CSS_file="css/main.css"
+
+# Check is Sass is installed
+if which sass >/dev/null; then
+  echo "Reading "$SASS_file" and compiling to "$CSS_file"..."
+  `sass "$SASS_file":"$CSS_file"` &
+  echo "Now watching changes in "$SASS_file" and compiling to "$CSS_file"..."
+  `sass --watch "$SASS_file":"$CSS_file"` &
+  wait
+fi
+else
+  sudo gem install sass &
+  wait
+  sass -v &
+  echo "Sass is now installed. Please run the script again."
+fi
+ 
