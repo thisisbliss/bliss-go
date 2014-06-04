87557cddd2bbd270fc1345d4d4a334abdca42779
Tom Wilks <tom@thisisbliss.com>
2014-06-04 15:48:31 +0100

    Updates Sass structure and partials. Removes bourbon.

--- template.php
+++ template.php
@@ -46,6 +46,33 @@ function blissgo_preprocess_page(&$vars, $hook) {
   //   '),
   //   'viewport',
   // );
+
+  // only do this for page-type nodes and only if Path module exists
+
+  // Page template suggestions off alias
+  if (module_exists('path') && isset($vars['node']) && $vars['node']->type == 'page') {
+    // look up the alias from the url_alias table
+    $source = 'node/' .$vars['node']->nid;
+    $alias = db_query("SELECT alias FROM {url_alias} WHERE source = '$source'")->fetchField();
+
+    if ($alias != '')  {
+      // build a suggestion for every possibility
+      $parts = explode('/', $alias);
+      $suggestion = '';
+      foreach ($parts as $part) {
+        if ($suggestion == '') {
+          // first suggestion gets prefaced with 'page--'
+          $suggestion .= "page--$part";
+        } else {
+          // subsequent suggestions get appended
+          $suggestion .= "__$part";
+        }
+        // add the suggestion to the array
+        $vars['theme_hook_suggestions'][] = $suggestion;
+      }
+    }
+  }
+
 }
 
 /**
@@ -68,5 +95,22 @@ function blissgo_preprocess_node(&$vars) {
 function blissgo_css_alter(&$css) {
   unset($css[drupal_get_path('module', 'system') . '/system.theme.css']);
   unset($css[drupal_get_path('module', 'system') . '/system.menus.css']);
+  unset($css[drupal_get_path('module', 'system') . '/system.messages.css']);
 }
+
+/**
+ * Allows use of theme snippets - Example
+ */
+// function blissgo_theme() {
+//   return array(
+//     'header' => array(
+//       'arguments' => array(),
+//       'template' => 'templates/header',
+//     ),
+//     'footer' => array(
+//       'arguments' => array(),
+//       'template' => 'templates/footer',
+//     ),
+//   );
+// }
 
