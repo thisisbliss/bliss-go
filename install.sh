80f229db6b07b46396faa552b10048e2aca5c54d
Tom Wilks <tom@thisisbliss.com>
2016-02-08 12:15:59 +0000

    Adds check to install file to see if Gulp is installed globally.

--- install.sh
+++ install.sh
@@ -2,10 +2,15 @@
 # Install all nessecary node and gulp dependencies
 
 if which node >/dev/null; then
-  echo "Installing dependencies... password required for sudo"
-  sudo npm install
-  echo "Starting watch file..."
-  ./watch.sh
+  if which gulp >/dev/null; then
+    echo "Installing dependencies... password required for sudo"
+    sudo npm install
+    echo "Starting watch file..."
+    ./watch.sh
+  else
+    echo "Gulp is not installed globally... installing with sudo..."
+    sudo npm install -g gulp
+  fi
 else
   echo "Node is not installed. Install from https://nodejs.org/en/ and run again."
 fi
