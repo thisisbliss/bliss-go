a3fe7552b70aa8d1b78e11ad5b1df63518b77bbc
Tom Wilks <tom@wilks.co>
2016-12-16 16:27:17 +0000

    Remove watch file. Superseded by npm/gulp.

--- watch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Run a simple set of Sass commands.
-
-if which node >/dev/null; then
-  echo "Starting gulp watch task..."
-  gulp watch
-else
-  echo "Node is not installed. Install from https://nodejs.org/en/ and run again."
-fi
