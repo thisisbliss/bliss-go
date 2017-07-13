92b94495f93f7e4ec966c9499c53dbe878ea875a
Simon McKeon <simon@thisisbliss.com>
2017-07-13 12:48:47 +0100

    Adds a better explanation of modules

--- scripts/lib/entry.js
+++ scripts/lib/entry.js
@@ -1,7 +1,16 @@
-// import ./welcomeBanner
-var welcomeBanner = require('./welcome-banner');
+// import welcomeBanner from 'show-me.js', notice the lack of curcly braces,
+// meaning we're importing the default export.
+import welcomeBanner from './show-me';
 
-// Assign ./welcomeBanner to a Drupal behavoir
+// import sanityCheck from 'show-me.js', notice the curly braces, meaning we're
+// importing a non-default export.
+import { sanityCheck } from './show-me';
+
+// sanityCheck is just a function, we can use it like any other function
+sanityCheck();
+
+// welcomeBanner is a drupal behavior object, which means we can simply add it
+// to the behaviors object and all is well
 Drupal.behaviors = {
   welcomeBanner: welcomeBanner
 }
