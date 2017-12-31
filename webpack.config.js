89f31efd3b247444dcf1d9f904ad9d0e2fa15907
Simon McKeon <development.simon.mckeon@gmail.com>
2017-12-31 19:59:20 +0000

    Add documentation for webpack externals

--- webpack.config.js
+++ webpack.config.js
@@ -55,6 +55,20 @@ module.exports = {
    */
   devtool: 'source-map',
 
+  /**
+   * Externals are used to tell Webpack and/or linting that we expect a variable to
+   * exist in the global scope. This can not be linted, as it is outside of webpacks
+   * module scope, but it will stop linting from throwing errors relating to missing
+   * modules and will emaulte importing in webpack.
+   *
+   * e.g. We have access to Drupal in the globl scope, but for webpack and linting to
+   * know about this, we have to "pretend" to import it as follows:
+   * import Drupal from 'drupal
+   *
+   * other examples:
+   * import drupalTranslations from 'drupal-translations'
+   * import _ from 'underscore'
+   */
   externals: {
     drupal: 'Drupal',
     'drupal-settings': 'drupalSettings',
